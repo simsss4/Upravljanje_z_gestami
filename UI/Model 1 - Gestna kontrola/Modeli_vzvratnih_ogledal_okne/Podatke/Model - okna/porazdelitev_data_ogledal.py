60440f2511b5f0b2fb0fb3721a4bbac88f5208d4
MariaaaaJ <marija.jovanova1@student.um.si>
2025-05-18 19:50:05 +0200

    SCRUM-68 Skripta za porazdelitev podatkov na testne, učne in validacijske

---
+++ UI/Model 1 - Gestna kontrola/Modeli_vzvratnih_ogledal_okne/Podatke/Model - okna/porazdelitev_data_ogledal.py
@@ -0,0 +1,41 @@
+﻿import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.models import load_model
+
+# Naloži podatke
+X = np.load('X_mirrors.npy')  # vhodni podatki
+y = np.load('y_mirrors.npy')  # oznake
+
+# Preveri oblike podatkov
+print("X shape:", X.shape)
+print("y shape:", y.shape)
+
+# Prikaži število primerkov na razred
+unique, counts = np.unique(y, return_counts=True)
+print(dict(zip(unique, counts)))
+
+
+# Razdeli podatke na učne, validacijske in testne množice
+X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42) 
+
+# Preveri oblike deljenih podatkov
+print("Train shape:", X_train.shape, y_train.shape)
+print("Validation shape:", X_val.shape, y_val.shape)
+print("Test shape:", X_test.shape, y_test.shape)
+
+np.save('X_mirrors_train.npy', X_train)
+np.save('y_mirrors_train.npy', y_train)
+np.save('X_mirrors_val.npy', X_val)
+np.save('y_mirrors_val.npy', y_val)
+np.save('X_mirrors_test.npy', X_test)
+np.save('y_mirrors_test.npy', y_test)
+
+
+
+
+
