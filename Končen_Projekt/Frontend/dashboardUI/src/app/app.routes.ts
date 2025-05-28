bcb57f24c475af8008f39ac0c8833cd0af314f1c
terga17 <terglav10@gmail.com>
2025-05-28 22:39:11 +0200

    SCRUM-115 Ustvarjanje poti, app.routes.ts

--- Končen_Projekt/Frontend/dashboardUI/src/app/app.routes.ts
+++ Končen_Projekt/Frontend/dashboardUI/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from '../pages/dashboard/dashboard';
+import { DiagnostikaSistema } from '../pages/diagnostika-sistema/diagnostika-sistema';
+import { StatistikaModelov } from '../pages/statistika-modelov/statistika-modelov';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard,
+        title: 'Dashboard'
+    },
+    {
+        path: 'diagnostika',
+        component: DiagnostikaSistema,
+        title: 'Diagnostika sistema'
+    },
+    {
+        path: 'statistika',
+        component: StatistikaModelov,
+        title: 'Statistika modelov'
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
